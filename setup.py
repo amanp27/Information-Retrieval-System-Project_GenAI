5dc961c9145a49a5e0b7fac5ff72890d2d0eb747
Aman <amanprajapati22@gmail.com>
2025-08-25 02:24:46 +0530

    app.py created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Information Retrieval Project",
+    version = "0.0.0",
+    author = "Aman Prajapati",
+    author_email = "amanprajapati22@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+
+)
